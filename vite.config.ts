c85c3c6755bd53d5e992e2478008cd3f68398fce
dm-morozov <dem.morozov@gmail.com>
2026-01-20 18:11:45 +0500

    Выложили backend на Render

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // Указываем базовый путь для GitHub Pages
-  base: '/',
+  base: mode === 'production' ? '/netology_73_react-router/' : '/',
   plugins: [react()],
 }))
