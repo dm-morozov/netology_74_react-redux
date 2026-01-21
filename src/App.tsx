152f4d50b5c55ce0df2c662e652ef174651f19fb
Dmitriy Morozov (PC) <dem.morozov@gmail.com>
2026-01-21 15:19:35 +0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,8 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from './pages/Home'
-import About from './pages/About'
-import MainLayout from './layouts/MainLayout'
-import User from './pages/User'
-import NotFound from './pages/NotFound'
-import LayoutForDrive from './layouts/LayoutForDrive'
-import HomeForDrive from './components/NavigationMenu/HomeForDrive'
-import Drift from './components/NavigationMenu/Drift'
-import Timeattack from './components/NavigationMenu/Timeattack'
-import Forza from './components/NavigationMenu/Forza'
-import PostList from './components/CRUD/PostList'
-import PostNew from './components/CRUD/PostNew'
-import PostView from './components/CRUD/PostView'
-
 function App() {
   return (
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="user/:id" element={<User />} />
-
-        {/* Drive */}
-        <Route path="drive" element={<LayoutForDrive />}>
-          <Route index element={<HomeForDrive />} />
-          <Route path="drift" element={<Drift />} />
-          <Route path="timeattack" element={<Timeattack />} />
-          <Route path="forza" element={<Forza />} />
-        </Route>
-
-        {/* CRUD */}
-        <Route path="crud">
-          <Route index element={<PostList />} />
-          <Route path="posts">
-            <Route path="new" element={<PostNew />} />
-            <Route path=":id" element={<PostView />} />
-          </Route>
-        </Route>
-      </Route>
-
-      {/* 404 */}
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
   )
 }
 
--- src/components/CRUD/api.ts
+++
@@ -1,3 +0,0 @@
-const API_URL = 'https://netology-73-react-router.onrender.com/posts/'
-
-export default API_URL
