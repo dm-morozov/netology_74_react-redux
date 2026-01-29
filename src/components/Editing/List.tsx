e6ada36395b6c77e8ff2acc8b8582a7ee0225ecb
dm-morozov <dem.morozov@gmail.com>
2026-01-29 17:05:09 +0500

    feat: задача 'Фильтрация' решена. Вторая обязательная задача.

--- src/components/Editing/List.tsx
+++ src/components/Editing/List.tsx
@@ -1,33 +1,78 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { deleteItem, startEdit } from '../../store/actions'
-import { selectFilteredItems } from '../../store/selectors'
+import { selectFilteredItems, selectFilterStats } from '../../store/selectors'
+import type { State } from '../../store/types'
 
 const List = () => {
   const dispatch = useDispatch()
 
   const items = useSelector(selectFilteredItems)
+  const { found, total } = useSelector(selectFilterStats)
+
+  const searchTerm = useSelector((state: State) => state.filter.searchTerm)
+
+  const highlight = (text: string, search: string) => {
+    if (!search) return text
+
+    const regex = new RegExp(`(${search})`, 'ig')
+    return text
+      .split(regex)
+      .map((part, index) =>
+        part.toLowerCase() === search.toLowerCase() ? (
+          <mark key={index}>{part}</mark>
+        ) : (
+          part
+        ),
+      )
+  }
+
+  if (total === 0) {
+    return <p>Список пустой...</p>
+  }
+
+  if (found === 0) {
+    return (
+      <>
+        <p>Найдено 0 из {total}</p>
+        <p>Услуги не найдены...</p>
+      </>
+    )
+  }
 
   return (
-    <ul>
-      {items.map((item) => (
-        <li key={item.id} className="item">
-          <div>
-            <span className="item-name">{item.name}</span>
-            <span className="item-separator"> — </span>
-            <span className="item-price">{item.price} ₽</span>
-          </div>
-          <div>
-            <button onClick={() => dispatch(startEdit(item.id))}>
-              Редактировать
-            </button>
+    <div>
+      {total === found ? (
+        <p>
+          Всего записей: <strong>{total}</strong>
+        </p>
+      ) : (
+        <p>
+          Найдено <strong>{found}</strong> записи из <strong>{total}</strong>
+        </p>
+      )}
+      <ul>
+        {items.map((item) => (
+          <li key={item.id} className="item">
+            <div>
+              <span className="item-name">
+                {highlight(item.name, searchTerm)}
+              </span>
+              <span className="item-separator"> — </span>
+              <span className="item-price">{item.price} ₽</span>
+            </div>
+            <div>
+              <button onClick={() => dispatch(startEdit(item.id))}>
+                Редактировать
+              </button>
 
-            <button onClick={() => dispatch(deleteItem(item.id))}>
-              Удалить
-            </button>
-          </div>
-        </li>
-      ))}
-    </ul>
+              <button onClick={() => dispatch(deleteItem(item.id))}>
+                Удалить
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
--- src/components/Editing/SearchFilter.tsx
+++ src/components/Editing/SearchFilter.tsx
@@ -1,6 +1,6 @@
 // src/components/SearchFilter.tsx
 
-import { useRef, type ChangeEvent } from 'react'
+import { useEffect, useRef, useState, type ChangeEvent } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import type { State } from '../../store/types'
 import { clearSearch, setSearchTerm } from '../../store/actions'
@@ -11,30 +11,48 @@ const SearchFilter = () => {
 
   const searchTerm = useSelector((state: State) => state.filter.searchTerm)
 
+  // Чтобы инпут работал мгновенно и плавно,
+  // он должен быть привязан к "местной" переменной.
+  // Мы не беспокоим Redux, пока пользователь активно печатает.
+
+  const [localValue, setLocalValue] = useState(searchTerm)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      dispatch(setSearchTerm(localValue))
+    }, 300)
+
+    return () => clearTimeout(timeout)
+  }, [localValue, dispatch])
+
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
-    dispatch(setSearchTerm(event.target.value))
+    setLocalValue(event.target.value)
   }
 
   const handleClear = (): void => {
+    setLocalValue('')
     dispatch(clearSearch())
     inputRef.current?.focus()
   }
 
   return (
-    <div>
+    <div className="search-filter">
       <input
         type="text"
+        className="search-input"
+        name="search"
         ref={inputRef}
         placeholder="Поиск услуг..."
-        value={searchTerm}
+        value={localValue}
         onChange={handleChange}
       />
 
-      {searchTerm && (
-        <button onClick={handleClear} className="clear-btn">
-          ✕
-        </button>
-      )}
+      <button
+        onClick={handleClear}
+        className={`clear-btn ${!localValue ? 'is-hidden' : ''}`}
+      >
+        ✕
+      </button>
     </div>
   )
 }
